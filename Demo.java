c8a3f264334837fa7c70dac8d4ead98f950ac766
NilimaNV2024 <157456041+NilimaNV2024@users.noreply.github.com>
2026-01-18 07:02:14 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,18 @@
+class P{
+public static void main(String []args){
+System.out.println("P--class");
+}
+}
+
+
+class Q{
+public static void main(String []args){
+System.out.println("Q--class");
+}
+}
+
+class R{
+public static void main(String []args){
+System.out.println("R--class");
+}
+}
